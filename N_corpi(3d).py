7bfaef04c0d33863f12f007b0be1bb246fbf4727
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2022-01-01 22:34:22 +0100

    code transfer

---
+++ N_corpi(3d).py
@@ -0,0 +1,131 @@
+import numpy as np
+import random as rn
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+class Corpo:
+    '''Classe usata per creare i corpi
+    '''
+
+    def __init__(self, x, y, z, vx, vy, vz, m=1):
+        self.m = m
+        self.x = x
+        self.y = y
+        self.z = z
+        self.vx = vx
+        self.vy = vy
+        self.vz = vz
+
+    #aggiornamento posizione e velocità con eulero
+    def n_vel(self, fx, fy, fz, dt):
+        self.vx += fx*dt
+        self.vy += fy*dt
+        self.vz += fz*dt
+
+    def n_pos(self, dt):
+        self.x += self.vx*dt
+        self.y += self.vy*dt
+        self.z += self.vz*dt
+
+class Sistema:
+    '''
+    Classe per evoluzione del sistema.
+    Viene utilizzata la tecnica del softening  per impedire
+    divergenze nella foza, sp è il parametro di softening
+    '''
+
+    def __init__(self, corpi, G, sp=0):
+        self.corpi = corpi
+        self.G = G
+        self.sp = sp
+
+    def evolvo(self, dt):
+        '''
+        chimata ad ogni passo temporale, fa evolvere il sistema
+        solo di uno step dt, la forza è calcolata secondo la
+        legge di gravitazione universale;
+        '''
+
+        for corpo_1 in self.corpi:
+
+            fx = 0.0
+            fy = 0.0
+            fz = 0.0
+
+            for corpo_2 in self.corpi:
+                if corpo_1 != corpo_2:
+
+                    dx = corpo_2.x - corpo_1.x
+                    dy = corpo_2.y - corpo_1.y
+                    dz = corpo_2.z - corpo_1.z
+
+                    d = np.sqrt(dx**2 + dy**2 + dz**2 + self.sp)
+
+                    fx += self.G * corpo_2.m * dx / d**3
+                    fy += self.G * corpo_2.m * dy / d**3
+                    fz += self.G * corpo_2.m * dz / d**3
+
+            corpo_1.n_vel(fx, fy, fz, dt)
+
+        for corpo in self.corpi:
+            corpo.n_pos(dt)
+
+#numero di corpi, pari per come vengono creati
+N = 10
+C = []
+for n in range(N//2):
+    #vengono creati due copri alla volta con velocità uguale e opposta
+    #per mantenere l'impulso totale del sistema nullo
+    v_x = rn.uniform(-0.5, 0.5)
+    v_y = rn.uniform(-0.5, 0.5)
+    v_z = rn.uniform(-0.5, 0.5)
+    C.append(
+        Corpo(
+        rn.uniform(-0.5, 0.5), rn.uniform(-0.5, 0.5), rn.uniform(-0.5, 0.5),
+        v_x, v_y, v_z))
+    C.append(
+        Corpo(
+        rn.uniform(-0.5, 0.5), rn.uniform(-0.5, 0.5), rn.uniform(-0.5, 0.5),
+        -v_x, -v_y, -v_z))
+
+G = 1
+'''
+from scipy import constants as cst
+G = cst.value(u'Newtonian constant of gravitation')
+and so on for masses and distances for realistic simulation
+'''
+dim = 3 #il moto avviene nello spazio
+
+sist = Sistema(C, G, 0.01)
+
+dt = 1 / 10000
+T = int(1 / dt)
+X=np.zeros((dim, T, N))
+
+for t in range(T):
+    sist.evolvo(dt)
+    for n, corpo in zip(range(N), sist.corpi):
+        X[:, t, n] = corpo.x, corpo.y, corpo.z
+
+fig = plt.figure(2)
+ax = fig.gca(projection='3d')
+plt.title('Problema a n corpi', fontsize=15)
+plt.grid()
+ax.set_xlim(np.min(X[0,:,:]), np.max(X[0,:,:]))
+ax.set_ylim(np.min(X[1,:,:]), np.max(X[1,:,:]))
+ax.set_zlim(np.min(X[2,:,:]), np.max(X[2,:,:]))
+
+dot=np.array([])
+for corpo in sist.corpi:
+    dot=np.append(dot, plt.plot([], [], [], 'bo'))
+
+
+def animate(i):
+    for k in range(N):
+        dot[k].set_data_3d(X[0, i, k], X[1, i, k],  X[2, i, k])
+
+    return dot
+
+anim = animation.FuncAnimation(fig, animate, frames=range(0, T-1, 10), interval=1, blit=True, repeat=True)
+#anim.save('N_body.mp4', fps=160,  extra_args=['-vcodec', 'libx264'])
+plt.show()
---
+++ bin_sist.py
@@ -0,0 +1,124 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+class Corpo:
+    '''Classe usata per creare i corpi
+    '''
+
+    def __init__(self, x, y, vx, vy, m=1):
+        self.m = m
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+
+    #aggiornamento posizione e velocità con eulero
+    def n_vel(self, fx, fy, dt):
+        self.vx += fx*dt
+        self.vy += fy*dt
+
+    def n_pos(self, dt):
+        self.x += self.vx*dt
+        self.y += self.vy*dt
+
+class Sistema:
+    '''
+    Classe per evoluzione del sistema.
+    Viene utilizzata la tecnica del softening  per impedire
+    divergenze nella foza, sp è il parametro di softening
+    '''
+
+    def __init__(self, corpi, G, sp=0):
+        self.corpi = corpi
+        self.G = G
+        self.sp = sp
+
+    def evolvo(self, dt):
+        '''
+        chimata ad ogni passo temporale, fa evolvere il sistema
+        solo di uno step dt, la forza è calcolata secondo la
+        legge di gravitazione universale;
+        '''
+
+        for corpo_1 in self.corpi:
+
+            fx = 0.0
+            fy = 0.0
+
+            for corpo_2 in self.corpi:
+                if corpo_1 != corpo_2:
+
+                    dx = corpo_2.x - corpo_1.x
+                    dy = corpo_2.y - corpo_1.y
+
+                    d = np.sqrt(dx**2 + dy**2 + self.sp)
+
+                    fx += self.G * corpo_2.m * dx / d**3
+                    fy += self.G * corpo_2.m * dy / d**3
+
+            corpo_1.n_vel(fx, fy, dt)
+
+        for corpo in self.corpi:
+            corpo.n_pos(dt)
+
+
+#creazione dei corpi nella simulazione
+C1 = Corpo(0.5, 0, 0, 20, int(1e3))
+C2 = Corpo(-0.5, 0, 0, -20, int(1e3))
+C3 = Corpo(-1.5, 0, 0, 40, int(1e1))
+
+C = [C1, C2, C3]
+
+G = 1
+'''
+from scipy import constants as cst
+G = cst.value(u'Newtonian constant of gravitation')
+and so on for masses and distances for realistic simulation
+'''
+N = len(C)
+dim = 2 #il moto avviene su un piano
+
+
+sist = Sistema(C, G, 0.0)
+
+dt = 1/10000
+steps = int(1/dt)
+X = np.zeros((dim, steps, N))
+
+for t in range(steps):
+    sist.evolvo(dt)
+    for n, corpo in zip(range(N), sist.corpi):
+        X[:, t, n] = corpo.x, corpo.y
+
+fig = plt.figure(1)
+plt.title('Sistema binario con pianeta', fontsize=15)
+plt.grid()
+plt.xlim(np.min(X[0,:,:])-0.5, np.max(X[0,:,:])+0.5)
+plt.ylim(np.min(X[1,:,:])-0.5, np.max(X[1,:,:])+0.5)
+
+colors = plt.cm.jet(np.linspace(0, 1, N))
+
+dot=np.array([])
+line=np.array([])
+for c, corpo in zip(colors, sist.corpi):
+    dot=np.append(dot, plt.plot([], [], 'o', c=c))
+    line=np.append(line, plt.plot([], [], '-', c=c))
+
+
+def animate(i):
+    for k in range(N):
+        l_scia = 500 #linghezza della scia lascita sul grafico
+        if i>l_scia:
+            line[k].set_data(X[0, i-l_scia:i, k], X[1, i-l_scia:i, k])
+        else:
+            line[k].set_data(X[0, :i, k], X[1, :i, k])
+
+        dot[k].set_data(X[0, i, k], X[1, i, k])
+
+    return dot[0], dot[1], dot[2], line[0], line[1], line[2]
+
+anim = animation.FuncAnimation(fig, animate, frames=range(0, steps-1, 5), interval=10, blit=True, repeat=True)
+#anim.save('bin_sist.mp4', fps=160,  extra_args=['-vcodec', 'libx264'])
+
+plt.show()
